6f378beb89dba2fc97ab7d5804768e9b840ff101
Adrián <adrianespindev@gmail.com>
2023-11-19 11:50:21 +0100

    primer commit

---
+++ src/test/java/com/book/api/BookApi/BookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.api.BookApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
